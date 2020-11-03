e48ddfa146b66f1799de70193576cfa1c7765bdc
skyler.hart <skyler@skylerhart.com>
2020-11-03 11:16:53 -0500

    Added Get-UserGroup

--- WSTools.psm1
+++ WSTools.psm1
@@ -4219,6 +4219,22 @@ function Get-User {
 }
 
 
+function Get-UserGroup {
+<#
+.NOTES
+    Author: Skyler Hart
+    Created: 2020-11-03 11:14:26
+    Last Edit: 2020-11-03 11:14:26
+    Keywords:
+.LINK
+    https://wstools.dev
+#>
+    $id = [Security.Principal.WindowsIdentity]::GetCurrent()
+    $groups = $id.Groups | foreach-object {$_.Translate([Security.Principal.NTAccount])}
+    $groups | Select-Object Value -ExpandProperty Value
+}
+
+
 Function Get-LockedOutLocation {
 <#
 .SYNOPSIS
