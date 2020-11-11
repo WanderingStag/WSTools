5057381fb2259957bd09eb023deff0a4a664c3de
skyler.hart <skyler@skylerhart.com>
2020-11-10 19:04:35 -0500

    Added Convert-AppIconToBase64

--- WSTools.psm1
+++ WSTools.psm1
@@ -3872,6 +3872,52 @@ function Connect-RDP {
 New-Alias -Name "rdp" -Value Connect-RDP
 
 
+function Convert-AppIconToBase64 {
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.PARAMETER ComputerName
+    Specifies the name of one or more computers.
+.PARAMETER Path
+    Specifies a path to one or more locations.
+.EXAMPLE
+    C:\PS>Convert-AppIconToBase64
+    Example of how to use this cmdlet
+.EXAMPLE
+    C:\PS>Convert-AppIconToBase64 -PARAMETER
+    Another example of how to use this cmdlet but with a parameter or switch.
+.NOTES
+    Author: Skyler Hart
+    Created: 2020-11-10 18:57:12
+    Last Edit: 2020-11-10 18:57:12
+    Keywords: 
+    Other: 
+    Requires:
+        -Module ActiveDirectory
+        -PSSnapin Microsoft.Exchange.Management.PowerShell.Admin
+        -RunAsAdministrator
+.LINK
+    https://wstools.dev
+.LINK
+    https://www.skylerhart.com
+#>
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            HelpMessage = "Enter the path of the file to extract the icon from. Ex: C:\Temp\app.exe",
+            Mandatory=$true
+        )]
+        [ValidateNotNullOrEmpty()]
+        [string]$Path
+    )
+    Add-Type -AssemblyName System.Drawing
+    $Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($Path)
+    $Icon
+}
+
+
 function Convert-ImageToBase64 {
 <#
 .NOTES
@@ -3893,10 +3939,12 @@ function Convert-ImageToBase64 {
     )
 
     $b64 = [convert]::ToBase64String((get-content $ImagePath -encoding byte))
-    New-Object -TypeName PSObject -Property @{
-        Base64Value = $b64
-    }#new object
+    $b64
+    #New-Object -TypeName PSObject -Property @{
+    #    Base64Value = $b64
+    #}#new object
 }
+New-Alias -Name "Convert-ICOtoBase64" -Value Convert-ImageToBase64
 
 
 Function Open-Remedy {
