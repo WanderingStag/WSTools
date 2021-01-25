ac26ff62a4b3bdf1cab641f42672976ebfe5ba42
skyler.hart <skyler@skylerhart.com>
2021-01-25 13:48:04 -0500

    Added Uninstall-CiscoAnyConnect

--- WS_Remediation/WS_Remediation.psm1
+++ WS_Remediation/WS_Remediation.psm1
@@ -5065,6 +5065,112 @@ Function Uninstall-AdobeShockwave {
 New-Alias -Name "Uninstall-Shockwave" -Value Uninstall-AdobeShockwave
 
 
+Function Uninstall-CiscoAnyConnect {
+<#
+.NOTES
+    Author: Skyler Hart
+    Created: 2021-01-25 13:44:09
+    Last Edit: 2021-01-25 13:44:09
+    Requires:
+        -RunAsAdministrator
+.LINK
+    https://wstools.dev
+#>
+    [CmdletBinding()]
+    Param (
+        [Parameter(HelpMessage = "Enter one or more computer names separated by commas.",
+            Mandatory=$true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true
+        )]
+        [Alias('Host','Name','Computer','CN','ComputerName')]
+        [string[]]$ObjectList,
+
+        [Parameter()]
+        [int32]$MaxThreads = 5,
+
+        [Parameter()]
+        $SleepTimer = 200,
+
+        [Parameter()]
+        $MaxResultTime = 1200
+    )
+    Begin {
+        $ISS = [system.management.automation.runspaces.initialsessionstate]::CreateDefault()
+        $RunspacePool = [runspacefactory]::CreateRunspacePool(1, $MaxThreads, $ISS, $Host)
+        $RunspacePool.Open()
+        $Code = {
+            [CmdletBinding()]
+            Param (
+                [Parameter(
+                    Mandatory=$true,
+                    Position=0
+                )]
+                [string]$comp
+            )
+            try {
+                Get-WmiObject -Class Win32_Product -Filter "Name LIKE 'Cisco AnyConnect%'" -ComputerName $Comp -ErrorAction Stop | Remove-WmiObject -ErrorAction Stop
+                New-Object -TypeName PSObject -Property @{
+                    ComputerName = $comp
+                    Program = "Cisco AnyConnect"
+                    Status = "Removed"
+                }#new object
+            }#try
+            catch {
+                New-Object -TypeName PSObject -Property @{
+                    ComputerName = $comp
+                    Program = "Cisco AnyConnect"
+                    Status = "Failed"
+                }#new object
+            }#catch
+        }#end code block
+        $Jobs = @()
+    }
+    Process {
+        Write-Progress -Activity "Preloading threads" -Status "Starting Job $($jobs.count)"
+        ForEach ($Object in $ObjectList){
+            $PowershellThread = [powershell]::Create().AddScript($Code)
+            $PowershellThread.AddArgument($Object.ToString()) | out-null
+            $PowershellThread.RunspacePool = $RunspacePool
+            $Handle = $PowershellThread.BeginInvoke()
+            $Job = "" | Select-Object Handle, Thread, object
+            $Job.Handle = $Handle
+            $Job.Thread = $PowershellThread
+            $Job.Object = $Object.ToString()
+            $Jobs += $Job
+        }
+    }
+    End {
+        $ResultTimer = Get-Date
+        While (@($Jobs | Where-Object {$null -ne $_.Handle}).count -gt 0)  {
+            $Remaining = "$($($Jobs | Where-Object {$_.Handle.IsCompleted -eq $False}).object)"
+            If ($Remaining.Length -gt 60){
+                $Remaining = $Remaining.Substring(0,60) + "..."
+            }
+            Write-Progress `
+                -Activity "Waiting for Jobs - $($MaxThreads - $($RunspacePool.GetAvailableRunspaces())) of $MaxThreads threads running" `
+                -PercentComplete (($Jobs.count - $($($Jobs | Where-Object {$_.Handle.IsCompleted -eq $False}).count)) / $Jobs.Count * 100) `
+                -Status "$(@($($Jobs | Where-Object {$_.Handle.IsCompleted -eq $False})).count) remaining - $remaining"
+            ForEach ($Job in $($Jobs | Where-Object {$_.Handle.IsCompleted -eq $True})){
+                $Job.Thread.EndInvoke($Job.Handle)
+                $Job.Thread.Dispose()
+                $Job.Thread = $Null
+                $Job.Handle = $Null
+                $ResultTimer = Get-Date
+            }
+            If (($(Get-Date) - $ResultTimer).totalseconds -gt $MaxResultTime){
+                Write-Error "Child script appears to be frozen, try increasing MaxResultTime"
+                Exit
+            }
+            Start-Sleep -Milliseconds $SleepTimer
+        }
+        $RunspacePool.Close() | Out-Null
+        $RunspacePool.Dispose() | Out-Null
+    }
+}
+New-Alias -Name "Uninstall-AnyConnect" -Value Uninstall-CiscoAnyConnect
+
+
 Function Uninstall-GoogleChrome {
 <#
 .Notes
