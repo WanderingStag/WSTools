aee78572262241e68ebbf23148e380a61506ac85
skyler.hart <skyler@skylerhart.com>
2021-06-14 22:10:17 -0400

    Added Encase install to InstallRemote.ps1

--- InstallRemote.ps1
+++ InstallRemote.ps1
@@ -923,6 +923,72 @@ if (Test-Path $edge) {
     }
 }
 
+if (Test-Path $encase) {
+    $sv = $null
+    $ipv = $null
+    $install = $false
+    $pn = "Encase"
+    $sv = Get-Content $encase\SoftwareVersion.txt
+    $ipv = ($ip | Where-Object {$_.ProgramName -like "Encas*"} | Select-Object Version)[0].Version
+
+    if ($null -ne $ipv -or $ipv -ne "") {
+        $ipv = $ipv.Split('.')
+        $ipv = $ipv.Split(' ')
+    }
+    $sv = $sv.Split('.')
+    $sv = $sv.Split(' ')
+
+    #Determine if need to install
+    if ($null -ne $ipv -or $ipv -ne "") {
+        if ($sv[0] -gt $ipv[0]) {
+            $install = $true
+        }
+        elseif ($sv[0] -eq $ipv[0]) {
+            if ($sv[1] -gt $ipv[1]) {
+                $install = $true
+            }
+            elseif ($sv[1] -eq $ipv[1]) {
+                if ($sv[2] -gt $ipv[2]) {
+                    $install = $true
+                }
+                elseif ($sv[2] -eq $ipv[2]) {
+                    if ($sv[3] -gt $ipv[3]) {
+                        $install = $true
+                    }
+                    elseif ($sv[3] -eq $ipv[3]) {
+                        $install = $false
+                    }
+                    elseif ($sv[3] -lt $ipv[3]) {
+                        $install = $false
+                    }
+                }
+                elseif ($sv[2] -lt $ipv[2]) {
+                    $install = $false
+                }
+            }
+            elseif ($sv[1] -lt $ipv[1]) {
+                $install = $false
+            }
+        }
+        elseif ($sv[0] -lt $ipv[0]) {
+            $install = $false
+        }
+    }#if already installed
+    else {
+        $install = $true
+    }
+
+    #Install or not
+    if ($install -eq $true) {
+        Write-Output "$cn`: Installing $pn."
+        Start-Process $encase\Deploy-application.exe -ArgumentList "-DeployMode 'NonInteractive'" -NoNewWindow -Wait
+        Start-Sleep 300
+    }
+    else {
+        Write-Output "$cn`: $pn same as installed version or older. Skipping..."
+    }
+}
+
 if (Test-Path $firefox) {
     $sv = $null
     $ipv = $null
