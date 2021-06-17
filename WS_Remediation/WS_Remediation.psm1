2824da03929a02d46a2f8b8520f4211fc23d272f
skyler.hart <skyler@skylerhart.com>
2021-06-16 21:58:17 -0400

    Added function Initialize -GPUpdate Fixes #41

--- WS_Remediation/WS_Remediation.psm1
+++ WS_Remediation/WS_Remediation.psm1
@@ -8133,6 +8133,73 @@ function Enable-TLS1.2 { #DevSkim: ignore DS169125,DS440000
 }
 
 
+function Initialize-GPUpdate {
+<#
+.NOTES
+    Author: Skyler Hart
+    Created: 2021-06-16 21:41:53
+    Last Edit: 2021-06-16 21:41:53
+    Keywords:
+    Requires:
+        -RunAsAdministrator
+.LINK
+    https://wstools.dev
+.LINK
+    https://www.skylerhart.com
+#>
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            #HelpMessage = "Enter one or more computer names separated by commas.",
+            Mandatory=$false,
+            #Position=0,
+            #ValueFromPipelineByPropertyName = $true,
+            ValueFromPipeline = $true
+        )]
+        [Alias('Host','Name','Computer','CN')]
+        [string[]]$ComputerName
+    )
+
+    if ($null -eq $ComputerName -or $ComputerName -eq "") {
+        gpupdate.exe /force
+    }
+    else {
+        $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+        if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+            $i = 0
+            $number = $ComputerName.length
+            foreach ($Comp in $ComputerName) {
+                #Progress Bar
+                if ($number -gt "1") {
+                    $i++
+                    $amount = ($i / $number)
+                    $perc1 = $amount.ToString("P")
+                    Write-Progress -activity "Forcing a GPUpdate" -status "Computer $i of $number. Percent complete:  $perc1" -PercentComplete (($i / $ComputerName.length)  * 100)
+                }#if length
+
+                try {
+                    $install = Invoke-WMIMethod -Class Win32_Process -ComputerName $Comp -Name Create -ArgumentList "cmd /c gpupdate /force" -ErrorAction Stop #DevSkim: ignore DS104456
+                    $info = New-Object -TypeName PSObject -Property @{
+                        ComputerName = $Comp
+                        Status = "GPUpdate Initialized"
+                    }#new object
+                }
+                catch {
+                    $end = Get-Date
+                    $info = New-Object -TypeName PSObject -Property @{
+                        ComputerName = $Comp
+                        Status = "Unable to initialize GPUpdate"
+                    }#new object
+                }
+                $info
+            }#foreach computer
+        }#if admin
+        else {Write-Error "Must be ran as admin when running against remote computers"}#not admin
+
+    }#else not local
+}
+
+
 Function Set-FeatureSettingsOverride {
 <#
 .Notes
