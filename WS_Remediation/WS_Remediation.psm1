529dd1b3c6da9c63e30d655b1e5d0d6c5383bf49
skyler.hart <skyler@skylerhart.com>
2021-06-20 01:09:47 -0400

    Added Set-FirefoxAutoUpdate

--- WS_Remediation/WS_Remediation.psm1
+++ WS_Remediation/WS_Remediation.psm1
@@ -8257,6 +8257,59 @@ Function Set-FeatureSettingsOverride {
 }
 
 
+Function Set-FirefoxAutoUpdate {
+<#
+.Notes
+    AUTHOR: Skyler Hart
+    CREATED: 2021-06-20 01:01:26
+    LASTEDIT: 2021-06-20 01:08:02
+    KEYWORDS:
+    REQUIRES:
+        #Requires -RunAsAdministrator
+.LINK
+    https://wstools.dev
+#>
+    [CmdletBinding()]
+    Param (
+        [Parameter(
+            Mandatory=$false,
+            Position=0
+        )]
+        [Alias('Host','Name','Computer','CN')]
+        [string[]]$ComputerName = "$env:COMPUTERNAME",
+
+        [Parameter()]
+        [Switch]$Enable
+    )
+
+    $v1 = 'DisableAppUpdate'
+    if ($Enable) {$d = 0}
+    else {$d = 1}
+
+    $i = 0
+    $number = $ComputerName.length
+
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        foreach ($comp in $ComputerName) {
+            #Progress Bar
+            if ($number -gt "1") {
+                $i++
+                $amount = ($i / $number)
+                $perc1 = $amount.ToString("P")
+                Write-Progress -activity "Setting Firefox Auto Update value" -status "Computer $i of $number. Percent complete:  $perc1" -PercentComplete (($i / $ComputerName.length)  * 100)
+            }#if length
+
+            ([Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, $comp)).CreateSubKey('SOFTWARE\Policies\Mozilla\Firefox')
+            $BaseKey = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, $comp)
+            $SubKey = $BaseKey.OpenSubKey('SOFTWARE\Policies\Mozilla\Firefox',$true)
+            $SubKey.SetValue($v1, $d, [Microsoft.Win32.RegistryValueKind]::DWORD)
+        }#foreach computer
+    }#if admin
+    else {Write-Error "Set-FirefoxAutoUpdate must be ran as administrator"}
+}
+
+
 function Set-MS15124 {
 <#
 .Notes
