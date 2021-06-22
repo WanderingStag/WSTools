87bedec188ca69ee9b13e38d517a352c05f21a8e
skyler.hart <skyler@skylerhart.com>
2021-06-22 17:54:18 -0400

    Added function Open-NetLogonLog

--- WS_DomainMgmt/WS_DomainMgmt.psm1
+++ WS_DomainMgmt/WS_DomainMgmt.psm1
@@ -716,6 +716,48 @@ Function Open-LAPS {
 New-Alias -Name "laps" -Value Open-LAPS
 
 
+function Open-NetLogonLog {
+<#
+.NOTES
+    Author: Skyler Hart
+    Created: 2021-06-22 17:50:31
+    Last Edit: 2021-06-22 17:50:31
+    Keywords:
+.LINK
+    https://wstools.dev
+#>
+    $Continue = $false
+    $file = "$env:windir\debug\netlogon.log"
+    if (Test-Path "c:\Windows\ccm\CMTrace.exe") {
+        $app = "c:\Windows\ccm\CMTrace.exe"
+        $Continue = $true
+    }
+    elseif (Test-Path "C:\ProgramData\OSI\CMTrace.exe") {
+        $app = "C:\ProgramData\OSI\CMTrace.exe"
+        $Continue = $true
+    }
+    elseif (Test-Path "J:\Patches\CMTrace.exe") {
+        $app = "J:\Patches\CMTrace.exe"
+        $Continue = $true
+    }
+    else {
+        Write-Error "Cannot find CMTrace.exe"
+        $Continue = $false
+    }
+
+    if ($Continue) {
+        foreach ($file in $FileName) {
+            try {
+                Start-Process $app -ArgumentList $file -ErrorAction Stop
+            }
+            catch {
+                Write-Error "Could not find or did not have permission to open $file"
+            }
+        }
+    }
+}
+
+
 Function Open-SharedFolders {
 <#
    .Notes
