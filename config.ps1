13a4941d5fe15b90475e4027f1602cb3613af076
Skyer Hart <skyler@skylerhart.com>
2021-07-21 23:34:38 -0400

    Added SplunkUF and VMwareTools config items

--- config.ps1
+++ config.ps1
@@ -5,7 +5,7 @@
 )]
 
 $Global:WSToolsConfig = New-Object -TypeName PSObject -Property @{
-    # WSTools config v1.2.2
+    # WSTools config v1.2.3
     # Remove the # symbol infront of a line to enable it
     ###########################################################################################################################################
     #Application Service Names - used in Stop-AppService. Uses match to match the name of a service that is running and stops it.
@@ -119,9 +119,11 @@ $Global:WSToolsConfig = New-Object -TypeName PSObject -Property @{
     Edge = "J:\Patches\Edge"
     Git = "J:\Patches\GitSCM"
     IE11 = "J:\Patches\IE\IE11"
+    SplunkUF = "J:\Patches\Splunk_Forwarder"
     SysInternals = "J:\Tools\SysinternalsSuite"
     VSCode = "J:\Patches\VisualStudioCode"
     VLC = "J:\Tools\VLC"
+    VMwareTools = "J:\Patches\VMware_Tools"
     Wireshark = "J:\Tools\Wireshark"
     WMF3 = "J:\Microsoft\Windows Management Framework 3.0"
     WMF4 = "J:\Microsoft\Windows Management Framework 4.0"
