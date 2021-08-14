1af7733691826f1bc2a743ac949b122b3c9daa0c
Skyer Hart <skyler@skylerhart.com>
2021-08-13 22:01:43 -0400

    Added function Get-SplunkStatus

--- WS_Remediation/WS_Remediation.psm1
+++ WS_Remediation/WS_Remediation.psm1
@@ -9643,7 +9643,7 @@ New-Alias -Name "Set-NLA" -Value Set-NetworkLevelAuthentication
 ###########################################################################
 ###########################################################################
 ##                                                                       ##
-##                                 SCCM                                  ##
+##                              MECM/SCCM                                ##
 ##                                                                       ##
 ###########################################################################
 ###########################################################################
@@ -9957,9 +9957,6 @@ software updates for compliance assessment and whether there are updates to the 
 ##                                                                       ##
 ###########################################################################
 ###########################################################################
-
-
-
 Function Get-ENSStatus {
 <#
 .Notes
@@ -10577,5 +10574,68 @@ function Sync-HBSSWithServer {
 New-Alias -Name "Sync-HBSS" -Value Sync-HBSSWithServer
 New-Alias -Name "Sync-ENS" -Value Sync-HBSSWithServer
 New-Alias -Name "Sync-ESS" -Value Sync-HBSSWithServer
+
+
+###########################################################################
+###########################################################################
+##                                                                       ##
+##                                Splunk                                 ##
+##                                                                       ##
+###########################################################################
+###########################################################################
+function Get-SplunkStatus {
+<#
+.NOTES
+    Author: Skyler Hart
+    Created: 2021-08-13 21:52:22
+    Last Edit: 2021-08-13 21:52:22
+    Keywords:
+    Other:
+        -RunAsAdministrator
+.LINK
+    https://wstools.dev
+.LINK
+    https://www.skylerhart.com
+#>
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            Mandatory=$false,
+            Position=0
+        )]
+        [Alias('Host','Name','Computer','CN')]
+        [string[]]$ComputerName = "$env:COMPUTERNAME"
+    )
+
+    if ($ComputerName -eq $env:COMPUTERNAME) {
+        $info = Get-Service -Name SplunkForwarder -ComputerName $comp
+        New-Object -TypeName PSObject -Property @{
+            ComputerName = $comp
+            SplunkStatus = ($info.Status)
+        }#new object
+    }
+    else {
+        $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+        if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+            $i = 0
+            $number = $ComputerName.length
+            foreach ($Comp in $ComputerName) {
+                #Progress Bar
+                if ($number -gt "1") {
+                    $i++
+                    $amount = ($i / $number)
+                    $perc1 = $amount.ToString("P")
+                    Write-Progress -activity "Getting status of Splunk Service" -status "Computer $i ($comp) of $number. Percent complete:  $perc1" -PercentComplete (($i / $ComputerName.length)  * 100)
+                }#if length
+                $info = Get-Service -Name SplunkForwarder -ComputerName $comp
+                New-Object -TypeName PSObject -Property @{
+                    ComputerName = $comp
+                    SplunkStatus = ($info.Status)
+                }#new object
+            }
+        }
+        else {Write-Error "Must be ran as administrator"}
+    }
+}
 
 Export-ModuleMember -Alias * -Function *
