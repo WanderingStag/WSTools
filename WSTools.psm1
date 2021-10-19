288f0bce04c2f619db46060694f0e988b0573193
Skyer Hart <skyler@skylerhart.com>
2021-10-19 19:32:06 -0400

    Added Get-LinesOfCode

--- WSTools.psm1
+++ WSTools.psm1
@@ -2043,6 +2043,31 @@ function Get-IPrange {
 }
 
 
+function Get-LinesOfCode {
+<#
+.NOTES
+    Author: Skyler Hart
+    Created: 2021-10-19 19:10:36
+    Last Edit: 2021-10-19 19:10:36
+    Keywords:
+    Other: Excludes blank lines
+.LINK
+    https://wstools.dev
+#>
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            HelpMessage = "Enter the path of the folder you want to count lines of PowerShell and JSON code for",
+            Mandatory=$true
+        )]
+        [ValidateNotNullOrEmpty()]
+        [string]$Path
+    )
+
+    (Get-ChildItem -Path $Path -Recurse | Where-Object {$_.extension -in '.ps1','.psm1','.psd1','.json'} | select-string "^\s*$" -notMatch).Count
+}
+
+
 Function Get-LockedOutLocation {
 <#
 .SYNOPSIS
