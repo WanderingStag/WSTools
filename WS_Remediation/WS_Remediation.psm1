c89bc3333de4494b8e85b71ac642c2a0c9508326
Skyer Hart <skyler@skylerhart.com>
2021-10-19 10:39:56 -0400

    Added RestrictDriverInstallationToAdministrators

--- WS_Remediation/WS_Remediation.psm1
+++ WS_Remediation/WS_Remediation.psm1
@@ -9084,7 +9084,7 @@ function Set-PrintNightmareFix {
 .NOTES
     Author: Skyler Hart
     Created: 2021-07-14 20:47:02
-    Last Edit: 2021-07-14 20:47:02
+    Last Edit: 2021-10-19 10:39:03
     Requires:
         -RunAsAdministrator
 .LINK
@@ -9107,7 +9107,9 @@ function Set-PrintNightmareFix {
 
     $v1 = 'NoWarningNoElevationOnInstall'
     $v2 = 'UpdatePromptSettings'
+    $v3 = 'RestrictDriverInstallationToAdministrators'
     $d0 = 0
+    $d1 = 1
 
     $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
     if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
@@ -9120,6 +9122,10 @@ function Set-PrintNightmareFix {
             $BaseKey = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, $comp)
             $SubKey = $BaseKey.OpenSubKey('SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint',$true)
             $SubKey.SetValue($v2, $d0, [Microsoft.Win32.RegistryValueKind]::DWORD)
+
+            $BaseKey = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey([Microsoft.Win32.RegistryHive]::LocalMachine, $comp)
+            $SubKey = $BaseKey.OpenSubKey('SOFTWARE\Policies\Microsoft\Windows NT\Printers\PointAndPrint',$true)
+            $SubKey.SetValue($v3, $d1, [Microsoft.Win32.RegistryValueKind]::DWORD)
 
             if ($Comp -eq $env:COMPUTERNAME) {
                 if ($DisableSpooler) {
