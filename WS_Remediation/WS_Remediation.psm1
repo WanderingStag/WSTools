b21495703d57060439c117767c69bb412997b9a8
Skyer Hart <skyler@skylerhart.com>
2021-10-19 11:31:27 -0400

    Added function Set-HiveNightmareFix

--- WS_Remediation/WS_Remediation.psm1
+++ WS_Remediation/WS_Remediation.psm1
@@ -9079,6 +9079,29 @@ function Set-MS15124 {
 }#function set-ms15-124
 
 
+function Set-HiveNightmareFix {
+<#
+.NOTES
+    Author: Skyler Hart
+    Created: 2021-10-19 11:25:39
+    Last Edit: 2021-10-19 11:25:39
+    Keywords:
+    Requires:
+        -RunAsAdministrator
+.LINK
+    https://wstools.dev
+.LINK
+    https://www.skylerhart.com
+#>
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        icacls $env:windir\system32\config\*.* /inheritance:e
+        vssadmin.exe delete shadows /all
+    }
+    else {Write-Error "Must be ran as admin"}
+}
+
+
 function Set-PrintNightmareFix {
 <#
 .NOTES
