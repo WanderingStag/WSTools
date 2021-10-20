950c31b171a62321719c9f029b7b1b4be6bf5dc4
Skyer Hart <skyler@skylerhart.com>
2021-10-19 20:26:19 -0400

    Added Get-ModuleCommandCount

--- WSTools.psm1
+++ WSTools.psm1
@@ -2158,6 +2158,34 @@ Function Get-LockedOutLocation {
 }#end function
 
 
+function Get-ModuleCommandCount {
+<#
+.NOTES
+    Author: Skyler Hart
+    Created: 2021-10-19 19:50:28
+    Last Edit: 2021-10-19 19:50:28
+    Keywords:
+.LINK
+    https://wstools.dev
+#>
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            HelpMessage = "Enter the name of the module. It must be one that is imported.",
+            Mandatory=$true
+        )]
+        [ValidateNotNullOrEmpty()]
+        [Alias('Module')]
+        [string]$Name,
+
+        [switch]$Functions
+    )
+
+    if ($Functions) {(Get-Command -Module $Name -CommandType Function).Count}
+    else {(Get-Command -Module $Name).Count}
+}
+
+
 function Get-ModuleList {
 <#
 .SYNOPSIS
