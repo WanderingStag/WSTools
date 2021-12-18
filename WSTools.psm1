823a18f9e41da329a355b36103c9c995e22a7606
Skyer Hart <skyler@skylerhart.com>
2021-12-17 23:13:24 -0500

    Added Save-HelpToFile

--- WSTools.psm1
+++ WSTools.psm1
@@ -4718,6 +4718,39 @@ function Save-MaintenanceReport {
 }
 
 
+function Save-HelpToFile {
+<#
+.NOTES
+    Author: Skyler Hart
+    Created: 2021-12-17 23:05:01
+    Last Edit: 2021-12-17 23:05:01
+    Keywords:
+    Other:
+    Requires:
+        -RunAsAdministrator
+.LINK
+    https://wstools.dev
+#>
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [Alias('Path','Folder','Destination')]
+        [string]$DestinationPath
+    )
+
+    if ([string]::IsNullOrWhiteSpace($Source)) {
+        $DestinationPath = ($Global:WSToolsConfig).HelpFolder
+    }
+
+    if (Test-Path $DestinationPath) {
+        Save-Help -DestinationPath $DestinationPath -Module * -Force
+    }
+    else {
+        Write-Error 'Destination folder "$DestinationPath" not found.'
+    }
+}
+
+
 function Save-UpdateHistory {
 <#
 .NOTES
