86efe237a5c70f7c3d1d7e09241a716f9c88b211
Skyer Hart <skyler@skylerhart.com>
2021-12-19 19:12:46 -0500

    Get-FileMetaData you can now specify a single file

--- WSTools.psm1
+++ WSTools.psm1
@@ -1537,41 +1537,53 @@ Function Get-FileMetaData {
     returns a custom PSObject object that can be sorted, filtered or otherwise
     manipulated.
 .Example
-    Get-FileMetaData -folder "e:\music"
+    Get-FileMetaData -Path "e:\music"
     Gets file metadata for all files in the e:\music directory
 .Example
-    Get-FileMetaData -folder (gci e:\music -Recurse -Directory).FullName
+    Get-FileMetaData -Path (gci e:\music -Recurse -Directory).FullName
     This example uses the Get-ChildItem cmdlet to do a recursive lookup of
     all directories in the e:\music folder and then it goes through and gets
     all of the file metada for all the files in the directories and in the
     subdirectories.
 .Example
-    Get-FileMetaData -folder "c:\fso","E:\music\Big Boi"
+    Get-FileMetaData -Path "c:\fso","E:\music\Big Boi"
     Gets file metadata from files in both the c:\fso directory and the
     e:\music\big boi directory.
 .Example
-    $meta = Get-FileMetaData -folder "E:\music"
+    $meta = Get-FileMetaData -Path "E:\music"
     This example gets file metadata from all files in the root of the
     e:\music directory and stores the returned custom objects in a $meta
     variable for later processing and manipulation.
-.Parameter Folder
-    The folder that is parsed for files
+.Parameter Path
+    The path that is parsed for files
 .Notes
     NAME:  Get-FileMetaData
     AUTHOR: ed wilson, msft
-    LASTEDIT: 01/24/2014 14:08:24
+    Edited By: Skyler Hart
+    Original: 01/24/2014 14:08:24
+    Last Edit: 2021-12-19 18:54:58
     KEYWORDS: Storage, Files, Metadata
     HSG: HSG-2-5-14
 .Link
     https://devblogs.microsoft.com/scripting/
 #Requires -Version 2.0
 #>
-    Param([string[]]$folder)
-    foreach($sFolder in $folder) {
+    Param([string[]]$Path)
+    foreach($sFolder in $Path) {
+        $ItemInfo = Get-Item $sFolder | Select-Object *
+        if ($ItemInfo.Mode -like "d-*") {
+            $ItemType = "Directory"
+            $FolderPath = $sFolder
+        }
+        else {
+            $ItemType = "File"
+            $FolderPath = $ItemInfo.DirectoryName
+            $FileName = $ItemInfo.Name
+        }
         $a = 0
         $objShell = New-Object -ComObject Shell.Application
-        $objFolder = $objShell.namespace($sFolder)
-        foreach ($File in $objFolder.items()) {
+        $objFolder = $objShell.namespace($FolderPath)
+        $Metadata = foreach ($File in $objFolder.items()) {
             $FileMetaData = New-Object PSCustomObject
             for ($a ; $a  -le 266; $a++) {
                 if($objFolder.getDetailsOf($File, $a)) {
@@ -1583,6 +1595,12 @@ Function Get-FileMetaData {
             $a=0
             $FileMetaData
         } #end foreach $file
+        if ($ItemType -eq "File") {
+            $Metadata | Where-Object {$_.FileName -eq $FileName}
+        }
+        else {
+            $Metadata
+        }
     } #end foreach $sfolder
 } #end Get-FileMetaData
 
