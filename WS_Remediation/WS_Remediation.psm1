0e51b1c921e838d456a45bf5ac42336dc748710b
Skyer Hart <skyler@skylerhart.com>
2022-04-19 11:21:18 -0400

    Added function Uninstall-DamewareMiniRemoteControl

--- WS_Remediation/WS_Remediation.psm1
+++ WS_Remediation/WS_Remediation.psm1
@@ -6823,6 +6823,116 @@ Function Uninstall-CiscoAnyConnect {
 New-Alias -Name "Uninstall-AnyConnect" -Value Uninstall-CiscoAnyConnect
 
 
+Function Uninstall-DamewareMiniRemoteControl {
+<#
+.NOTES
+    Author: Skyler Hart
+    Created: 2022-04-19 11:19:52
+    Last Edit: 2022-04-19 11:19:52
+    Requires:
+        -RunAsAdministrator
+.LINK
+    https://wstools.dev
+#>
+    [CmdletBinding()]
+    Param (
+        [Parameter(
+            HelpMessage = "Enter one or more computer names separated by commas.",
+            Mandatory=$false,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true
+        )]
+        [Alias('Host','Name','Computer','CN','ComputerName')]
+        [string[]]$ObjectList,
+
+        [Parameter()]
+        [int32]$MaxThreads = 5,
+
+        [Parameter()]
+        $SleepTimer = 200,
+
+        [Parameter()]
+        $MaxResultTime = 1200
+    )
+
+    Begin {
+        if ([string]::IsNullOrWhiteSpace($ObjectList)) {
+            $ObjectList = $env:COMPUTERNAME
+        }
+        $ISS = [system.management.automation.runspaces.initialsessionstate]::CreateDefault()
+        $RunspacePool = [runspacefactory]::CreateRunspacePool(1, $MaxThreads, $ISS, $Host)
+        $RunspacePool.Open()
+        $Code = {
+            [CmdletBinding()]
+            Param (
+                [Parameter(
+                    Mandatory=$true,
+                    Position=0
+                )]
+                [string]$comp
+            )
+            try {
+                Get-WmiObject -Class Win32_Product -Filter "Name LIKE 'Dameware Mini Remote%'" -ComputerName $Comp -ErrorAction Stop | Remove-WmiObject -ErrorAction Stop
+                [PSCustomObject]@{
+                    ComputerName = $comp
+                    Program = "Dameware Mini Remote Control"
+                    Status = "Removed"
+                }#new object
+            }#try
+            catch {
+                [PSCustomObject]@{
+                    ComputerName = $comp
+                    Program = "Dameware Mini Remote Control"
+                    Status = "Failed"
+                }#new object
+            }#catch
+        }#end code block
+        $Jobs = @()
+    }
+    Process {
+        Write-Progress -Activity "Preloading threads" -Status "Starting Job $($jobs.count)"
+        ForEach ($Object in $ObjectList){
+            $PowershellThread = [powershell]::Create().AddScript($Code)
+            $PowershellThread.AddArgument($Object.ToString()) | out-null
+            $PowershellThread.RunspacePool = $RunspacePool
+            $Handle = $PowershellThread.BeginInvoke()
+            $Job = "" | Select-Object Handle, Thread, object
+            $Job.Handle = $Handle
+            $Job.Thread = $PowershellThread
+            $Job.Object = $Object.ToString()
+            $Jobs += $Job
+        }
+    }
+    End {
+        $ResultTimer = Get-Date
+        While (@($Jobs | Where-Object {$null -ne $_.Handle}).count -gt 0)  {
+            $Remaining = "$($($Jobs | Where-Object {$_.Handle.IsCompleted -eq $False}).object)"
+            If ($Remaining.Length -gt 60){
+                $Remaining = $Remaining.Substring(0,60) + "..."
+            }
+            Write-Progress `
+                -Activity "Waiting for Jobs - $($MaxThreads - $($RunspacePool.GetAvailableRunspaces())) of $MaxThreads threads running" `
+                -PercentComplete (($Jobs.count - $($($Jobs | Where-Object {$_.Handle.IsCompleted -eq $False}).count)) / $Jobs.Count * 100) `
+                -Status "$(@($($Jobs | Where-Object {$_.Handle.IsCompleted -eq $False})).count) remaining - $remaining"
+            ForEach ($Job in $($Jobs | Where-Object {$_.Handle.IsCompleted -eq $True})){
+                $Job.Thread.EndInvoke($Job.Handle)
+                $Job.Thread.Dispose()
+                $Job.Thread = $Null
+                $Job.Handle = $Null
+                $ResultTimer = Get-Date
+            }
+            If (($(Get-Date) - $ResultTimer).totalseconds -gt $MaxResultTime){
+                Write-Error "Child script appears to be frozen, try increasing MaxResultTime"
+                Exit
+            }
+            Start-Sleep -Milliseconds $SleepTimer
+        }
+        $RunspacePool.Close() | Out-Null
+        $RunspacePool.Dispose() | Out-Null
+    }
+}
+
+
 Function Uninstall-F5BigIPEdgeClient {
 <#
 .NOTES
