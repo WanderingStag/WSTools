1f3385a1d5b01c6eaa3a0ad98e28f0d10f9d86ec
Skyer Hart <skyler@skylerhart.com>
2022-06-04 22:47:59 -0400

    Added function Get-HWPerformanceScore

--- WSTools.psm1
+++ WSTools.psm1
@@ -1982,6 +1982,98 @@ function Get-HWInfo {
     else {Write-Error "Not admin. Please run PowerShell as admin."}
 }
 
+function Get-HWPerformanceScore {
+<#
+.NOTES
+    Author: Skyler Hart
+    Created: 2022-06-04 22:43:29
+    Last Edit: 2022-06-04 22:43:29
+    Other:
+    Requires:
+        -RunAsAdministrator
+.LINK
+    https://wstools.dev
+#>
+    [CmdletBinding()]
+    Param (
+        [Parameter(
+            Mandatory = $false,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true
+        )]
+        [Alias('Host','Name','Computer','CN')]
+        [string[]]$ComputerName = $env:COMPUTERNAME,
+
+        [Parameter()]
+        [int32]$MaxThreads = 5,
+
+        [Parameter()]
+        $SleepTimer = 200,
+
+        [Parameter()]
+        $MaxResultTime = 1200
+    )
+    Begin {
+        $ISS = [system.management.automation.runspaces.initialsessionstate]::CreateDefault()
+        $RunspacePool = [runspacefactory]::CreateRunspacePool(1, $MaxThreads, $ISS, $Host)
+        $RunspacePool.Open()
+        $Code = {
+            [CmdletBinding()]
+            Param (
+                [Parameter(
+                    Mandatory=$true,
+                    Position=0
+                )]
+                [string]$comp
+            )
+            Get-CimInstance -ClassName Win32_WinSAT -ComputerName $comp -ErrorAction Stop
+        }#end code block
+        $Jobs = @()
+    }
+    Process {
+        Write-Progress -Activity "Preloading threads" -Status "Starting Job $($jobs.count)"
+        ForEach ($Object in $ComputerName){
+            $PowershellThread = [powershell]::Create().AddScript($Code)
+            $PowershellThread.AddArgument($Object.ToString()) | out-null
+            $PowershellThread.AddArgument($app.ToString()) | out-null
+            $PowershellThread.RunspacePool = $RunspacePool
+            $Handle = $PowershellThread.BeginInvoke()
+            $Job = "" | Select-Object Handle, Thread, object
+            $Job.Handle = $Handle
+            $Job.Thread = $PowershellThread
+            $Job.Object = $Object.ToString()
+            $Jobs += $Job
+        }
+    }
+    End {
+        $ResultTimer = Get-Date
+        While (@($Jobs | Where-Object {$Null -ne $_.Handle}).count -gt 0)  {
+            $Remaining = "$($($Jobs | Where-Object {$_.Handle.IsCompleted -eq $False}).object)"
+            If ($Remaining.Length -gt 60){
+                $Remaining = $Remaining.Substring(0,60) + "..."
+            }
+            Write-Progress `
+                -Activity "Getting hardware performance scores. Waiting for Jobs - $($MaxThreads - $($RunspacePool.GetAvailableRunspaces())) of $MaxThreads threads running" `
+                -PercentComplete (($Jobs.count - $($($Jobs | Where-Object {$_.Handle.IsCompleted -eq $False}).count)) / $Jobs.Count * 100) `
+                -Status "$(@($($Jobs | Where-Object {$_.Handle.IsCompleted -eq $False})).count) remaining - $remaining"
+            ForEach ($Job in $($Jobs | Where-Object {$_.Handle.IsCompleted -eq $True})){
+                $Job.Thread.EndInvoke($Job.Handle)
+                $Job.Thread.Dispose()
+                $Job.Thread = $Null
+                $Job.Handle = $Null
+                $ResultTimer = Get-Date
+            }
+            If (($(Get-Date) - $ResultTimer).totalseconds -gt $MaxResultTime){
+                Write-Error "Child script appears to be frozen, try increasing MaxResultTime"
+                Exit
+            }
+            Start-Sleep -Milliseconds $SleepTimer
+        }
+        $RunspacePool.Close() | Out-Null
+        $RunspacePool.Dispose() | Out-Null
+    }
+}
+
 
 Function Get-IEVersion {
 <#
