43579fbe665cadd037a18d111efd39811ebca410
Skyer Hart <skyler@skylerhart.com>
2022-06-29 22:25:48 -0400

    Added Update-ModuleFromLocalRepo

--- WSTools.psm1
+++ WSTools.psm1
@@ -7002,6 +7002,161 @@ function Update-HelpFromFile {
 }
 
 
+function Update-ModuleFromLocalRepo {
+<#
+.NOTES
+    Author: Skyler Hart
+    Created: 2022-06-29 21:51:12
+    Last Edit: 2022-06-29 21:51:12
+    Other:
+    Requires:
+        -Module ActiveDirectory
+        -PSSnapin Microsoft.Exchange.Management.PowerShell.Admin
+        -RunAsAdministrator
+.LINK
+    https://wstools.dev
+#>
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+        "PSAvoidGlobalVars",
+        "",
+        Justification = "Have tried other methods and they do not work consistently."
+    )]
+    [CmdletBinding()]
+    Param (
+        [Parameter(
+            Mandatory = $false,
+            Position = 0,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true
+        )]
+        [Alias('Host','Name','Computer','CN')]
+        [string[]]$ComputerName = $env:COMPUTERNAME,
+
+        [Parameter()]
+        [int32]$MaxThreads = 5,
+
+        [Parameter()]
+        $SleepTimer = 200,
+
+        [Parameter()]
+        $MaxResultTime = 1200,
+
+        [Parameter()]
+        [string]$Name
+    )
+    Begin {
+        $config = $Global:WSToolsConfig
+        $repo = $config.LocalPSRepo
+
+        if ([string]::IsNullOrWhiteSpace($Name)) {
+            $Name = "All"
+        }
+
+        $ISS = [system.management.automation.runspaces.initialsessionstate]::CreateDefault()
+        $RunspacePool = [runspacefactory]::CreateRunspacePool(1, $MaxThreads, $ISS, $Host)
+        $RunspacePool.Open()
+        $Code = {
+            [CmdletBinding()]
+            Param (
+                [Parameter(
+                    Mandatory=$true,
+                    Position=0
+                )]
+                [string]$comp,
+
+                [Parameter(
+                    Mandatory=$true,
+                    Position=1
+                )]
+                [string]$repo,
+
+                [Parameter(
+                    Mandatory=$true,
+                    Position=2
+                )]
+                [string]$Name
+            )
+            $rmodules = Get-ChildItem $repo | Where-Object {$_.Attributes -eq "Directory"} | Select-Object Name,FullName
+            if ($comp -eq $env:COMPUTERNAME) {
+                if ([string]::IsNullOrWhiteSpace($Name) -or $Name -eq "All") {
+                    $lmodules = Get-ChildItem $env:ProgramFiles\WindowsPowerShell\Modules | Where-Object {$_.Attributes -eq "Directory"} | Select-Object Name,FullName
+                }
+                else {
+                    $lmodules = Get-ChildItem $env:ProgramFiles\WindowsPowerShell\Modules | Where-Object {$_.Attributes -eq "Directory" -and $_.Name -match $Name} | Select-Object Name,FullName
+                }
+            }#if local
+            else {
+                if ([string]::IsNullOrWhiteSpace($Name) -or $Name -eq "All") {
+                    $lmodules = Get-ChildItem "\\$comp\c$\Program Files\WindowsPowerShell\Modules" | Where-Object {$_.Attributes -eq "Directory"} | Select-Object Name,FullName
+                }
+                else {
+                    $lmodules = Get-ChildItem $env:ProgramFiles\WindowsPowerShell\Modules | Where-Object {$_.Attributes -eq "Directory" -and $_.Name -match $Name} | Select-Object Name,FullName
+                }
+            }#if remote
+
+            foreach ($mod in $lmodules) {
+                $modname = $mod.Name
+                $modpath = $mod.FullName
+
+                $rpath = $rmodules | Where-Object {$_.Name -eq $modname} | Select-Object -ExpandProperty FullName
+
+                if ([string]::IsNullOrWhiteSpace($rpath)) {
+                    #do nothing
+                }
+                else {
+                    Write-Output "$(Get-Date) - ${comp}: Updating $modname"
+                    robocopy $rpath $modpath /mir /mt:4 /njh /njs | Out-Null
+                }
+            }
+        }#end code block
+        $Jobs = @()
+    }
+    Process {
+        Write-Progress -Activity "Copying PowerShell modules" -Status "Starting Job $($jobs.count)"
+        ForEach ($Object in $ComputerName){
+            $PowershellThread = [powershell]::Create().AddScript($Code)
+            $PowershellThread.AddArgument($Object.ToString()) | out-null
+            $PowershellThread.AddArgument($repo.ToString()) | out-null
+            $PowershellThread.AddArgument($Name.ToString()) | out-null
+            $PowershellThread.RunspacePool = $RunspacePool
+            $Handle = $PowershellThread.BeginInvoke()
+            $Job = "" | Select-Object Handle, Thread, object
+            $Job.Handle = $Handle
+            $Job.Thread = $PowershellThread
+            $Job.Object = $Object.ToString()
+            $Jobs += $Job
+        }
+    }
+    End {
+        $ResultTimer = Get-Date
+        While (@($Jobs | Where-Object {$Null -ne $_.Handle}).count -gt 0)  {
+            $Remaining = "$($($Jobs | Where-Object {$_.Handle.IsCompleted -eq $False}).object)"
+            If ($Remaining.Length -gt 60){
+                $Remaining = $Remaining.Substring(0,60) + "..."
+            }
+            Write-Progress `
+                -Activity "Waiting for Jobs - $($MaxThreads - $($RunspacePool.GetAvailableRunspaces())) of $MaxThreads threads running" `
+                -PercentComplete (($Jobs.count - $($($Jobs | Where-Object {$_.Handle.IsCompleted -eq $False}).count)) / $Jobs.Count * 100) `
+                -Status "$(@($($Jobs | Where-Object {$_.Handle.IsCompleted -eq $False})).count) remaining - $remaining"
+            ForEach ($Job in $($Jobs | Where-Object {$_.Handle.IsCompleted -eq $True})){
+                $Job.Thread.EndInvoke($Job.Handle)
+                $Job.Thread.Dispose()
+                $Job.Thread = $Null
+                $Job.Handle = $Null
+                $ResultTimer = Get-Date
+            }
+            If (($(Get-Date) - $ResultTimer).totalseconds -gt $MaxResultTime){
+                Write-Error "Child script appears to be frozen, try increasing MaxResultTime"
+                Exit
+            }
+            Start-Sleep -Milliseconds $SleepTimer
+        }
+        $RunspacePool.Close() | Out-Null
+        $RunspacePool.Dispose() | Out-Null
+    }
+}
+
+
 function Update-VisioStencils {
 <#
 .NOTES
