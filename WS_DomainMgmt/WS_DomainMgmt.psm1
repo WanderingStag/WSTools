b0d0e001ebc2dba0ff1aaef5f044422d96a5f1c3
Skyer Hart <skyler@skylerhart.com>
2022-07-07 21:24:52 -0400

    Added Open-RackElevation

--- WS_DomainMgmt/WS_DomainMgmt.psm1
+++ WS_DomainMgmt/WS_DomainMgmt.psm1
@@ -1050,6 +1050,55 @@ function Open-PrintRelease {
 }
 
 
+function Open-RackElevation {
+<#
+.NOTES
+    Author: Skyler Hart
+    Created: 2022-07-07 21:22:25
+    Last Edit: 2022-07-07 21:22:25
+    Other:
+.LINK
+    https://wstools.dev
+#>
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+        "PSAvoidGlobalVars",
+        "",
+        Justification = "Have tried other methods and they do not work consistently."
+    )]
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$false)]
+        [Switch]$Chrome,
+
+        [Parameter(Mandatory=$false)]
+        [Switch]$Edge,
+
+        [Parameter(Mandatory=$false)]
+        [Switch]$Firefox,
+
+        [Parameter(Mandatory=$false)]
+        [Switch]$InternetExplorer
+    )
+
+    $config = $Global:WSToolsConfig
+    $dpath = $config.RackEl
+
+    if ($dpath -like "http*") {
+        if ($Chrome) {Start-Process "chrome.exe" $dpath}
+        elseif ($Edge) {Start-Process shell:AppsFolder\Microsoft.MicrosoftEdge_8wekyb3d8bbwe!MicrosoftEdge $dpath}
+        elseif ($Firefox) {Start-Process "firefox.exe" $dpath}
+        elseif ($InternetExplorer) {Start-Process "iexplore.exe" $dpath}
+        else {
+            #open in default browser
+            (New-Object -com Shell.Application).Open($dpath)
+        }
+    }#is web address
+    else {
+        Start-Process $dpath
+    }
+}
+
+
 function Open-SDN {
 <#
 .Notes
