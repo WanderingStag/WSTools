1ccb54c70f0ccb62739450d769f39155a4a6054c
Skyer Hart <skyler@skylerhart.com>
2022-07-07 21:21:03 -0400

    Added Open-NetworkDiagram

--- WS_DomainMgmt/WS_DomainMgmt.psm1
+++ WS_DomainMgmt/WS_DomainMgmt.psm1
@@ -888,6 +888,81 @@ function Open-NetLogonLog {
 }
 
 
+function Open-NetworkDiagram {
+<#
+.SYNOPSIS
+    Short description
+.DESCRIPTION
+    Long description
+.PARAMETER ComputerName
+    Specifies the name of one or more computers.
+.PARAMETER Path
+    Specifies a path to one or more locations.
+.EXAMPLE
+    C:\PS>Open-NetworkDiagram
+    Example of how to use this cmdlet
+.EXAMPLE
+    C:\PS>Open-NetworkDiagram -PARAMETER
+    Another example of how to use this cmdlet but with a parameter or switch.
+.INPUTS
+    System.String
+.OUTPUTS
+    System.Management.Automation.PSCustomObject
+.COMPONENT
+    WSTools
+.FUNCTIONALITY
+    The functionality (keywords) that best describes this cmdlet
+.NOTES
+    Author: Skyler Hart
+    Created: 2022-07-07 20:59:35
+    Last Edit: 2022-07-07 20:59:35
+    Other:
+    Requires:
+        -Module ActiveDirectory
+        -PSSnapin Microsoft.Exchange.Management.PowerShell.Admin
+        -RunAsAdministrator
+.LINK
+    https://wstools.dev
+#>
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+        "PSAvoidGlobalVars",
+        "",
+        Justification = "Have tried other methods and they do not work consistently."
+    )]
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$false)]
+        [Switch]$Chrome,
+
+        [Parameter(Mandatory=$false)]
+        [Switch]$Edge,
+
+        [Parameter(Mandatory=$false)]
+        [Switch]$Firefox,
+
+        [Parameter(Mandatory=$false)]
+        [Switch]$InternetExplorer
+    )
+
+    $config = $Global:WSToolsConfig
+    $dpath = $config.NetDiagram
+
+    if ($dpath -like "http*") {
+        if ($Chrome) {Start-Process "chrome.exe" $dpath}
+        elseif ($Edge) {Start-Process shell:AppsFolder\Microsoft.MicrosoftEdge_8wekyb3d8bbwe!MicrosoftEdge $dpath}
+        elseif ($Firefox) {Start-Process "firefox.exe" $dpath}
+        elseif ($InternetExplorer) {Start-Process "iexplore.exe" $dpath}
+        else {
+            #open in default browser
+            (New-Object -com Shell.Application).Open($dpath)
+        }
+    }#is web address
+    else {
+        Start-Process $dpath
+    }
+}
+
+
 function Open-OWA {
 <#
 .Notes
