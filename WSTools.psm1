a3bb757fae05fb03465eb95b6500fbb9148eb5c7
Skyer Hart <skyler@skylerhart.com>
2022-07-15 22:47:10 -0400

    Added local computer code Added help text

--- WSTools.psm1
+++ WSTools.psm1
@@ -840,35 +840,31 @@ function Enable-ServerManager {
 function Repair-DuplicateSusClientID {
 <#
 .SYNOPSIS
-    Short description
+    Removes SusClientID registry key on the local or remote computer.
 .DESCRIPTION
-    Long description
+    When creating a computer from a template (virtual disc) the SusClientID isn't changed and will result in WSUS only having one object for all the computers created. This function clears the SusClientID from the registry on the local or remote computer(s) so when syncing with WSUS a new SusClientID will be created. The first initial sync with WSUS typically fails. It may take several minutes for the computer to sync appropriately with WSUS.
 .PARAMETER ComputerName
     Specifies the name of one or more computers.
-.PARAMETER Path
-    Specifies a path to one or more locations.
 .EXAMPLE
     C:\PS>Repair-DuplicateSusClientID
-    Example of how to use this cmdlet
+    Example of how to use this cmdlet to fix a duplicate SusClientID on the local computer.
 .EXAMPLE
-    C:\PS>Repair-DuplicateSusClientID -PARAMETER
-    Another example of how to use this cmdlet but with a parameter or switch.
+    C:\PS>Repair-DuplicateSusClientID -ComputerName Server1
+    Another example of how to use this cmdlet but with the ComputerName parameter. In this example, Server1 is a remote computer.
 .INPUTS
     System.String
 .OUTPUTS
-    System.Management.Automation.PSCustomObject
+    System.String
 .COMPONENT
     WSTools
 .FUNCTIONALITY
-    The functionality (keywords) that best describes this cmdlet
+    WSUS, fix, repair, SusClientID
 .NOTES
     Author: Skyler Hart
     Created: 2022-07-15 21:05:27
     Last Edit: 2022-07-15 21:05:27
     Other:
     Requires:
-        -Module ActiveDirectory
-        -PSSnapin Microsoft.Exchange.Management.PowerShell.Admin
         -RunAsAdministrator
 .LINK
     https://wstools.dev
@@ -876,41 +872,55 @@ function Repair-DuplicateSusClientID {
     [CmdletBinding()]
     param(
         [Parameter(
-            #HelpMessage = "Enter one or more computer names separated by commas.",
-            Mandatory=$false#,
-            #Position=0,
-            #ValueFromPipelineByPropertyName = $true,
-            #ValueFromPipeline = $true
+            Mandatory=$false
         )]
-        [ValidateNotNullOrEmpty()]
-        [ValidateCount(min,max)]
-        [ValidateLength(min,max)]
-        [ValidateSet('Info','Error','Warning','One','Two','Three')]
         [Alias('Host','Name','Computer','CN')]
         [string[]]$ComputerName = "$env:COMPUTERNAME"
     )
 
-    Invoke-Command -ComputerName $ComputerName -ScriptBlock {#DevSkim: ignore DS104456
-        $comp = $env:COMPUTERNAME
-        Write-Output "$(Get-Date) - ${comp}: Stoppping Services"
+    if ($ComputerName -eq $env:COMPUTERNAME) {
+        Write-Output "$(Get-Date) - ${ComputerName}: Stoppping Services"
         Get-Service -Name BITS | Stop-Service
         Get-Service -Name wuauserv | Stop-Service
-        Write-Output "$(Get-Date) - ${comp}: Removing registry keys"
+        Write-Output "$(Get-Date) - ${ComputerName}: Removing registry keys"
         Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate" -Name "AccountDomainSid" -Force | Out-Null
         Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate" -Name "PingID" -Force | Out-Null
         Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate" -Name "SusClientId" -Force | Out-Null
         Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate" -Name "SusClientIdValidation" -Force | Out-Null
-        Write-Output "$(Get-Date) - ${comp}: Removing SoftwareDistribution folder"
+        Write-Output "$(Get-Date) - ${ComputerName}: Removing SoftwareDistribution folder"
         Remove-Item -Path C:\Windows\SoftwareDistribution -Force | Out-Null
-        Write-Output "$(Get-Date) - ${comp}: Starting Services"
+        Write-Output "$(Get-Date) - ${ComputerName}: Starting Services"
         Get-Service -Name BITS | Start-Service
         Get-Service -Name wuauserv | Start-Service
-        Write-Output "$(Get-Date) - ${comp}: Reauthorizing client"
+        Write-Output "$(Get-Date) - ${ComputerName}: Reauthorizing client"
         Start-Process -FilePath "C:\Windows\System32\wuauclt.exe" -ArgumentList "/resetauthorization /detectnow" -Wait
         Start-Sleep -Seconds 10
-        Write-Output "$(Get-Date) - ${comp}: Starting detection"
+        Write-Output "$(Get-Date) - ${ComputerName}: Starting detection"
         (New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow()
-    } -ThrottleLimit 5
+    }
+    else{
+        Invoke-Command -ComputerName $ComputerName -ScriptBlock {#DevSkim: ignore DS104456
+            $comp = $env:COMPUTERNAME
+            Write-Output "$(Get-Date) - ${comp}: Stoppping Services"
+            Get-Service -Name BITS | Stop-Service
+            Get-Service -Name wuauserv | Stop-Service
+            Write-Output "$(Get-Date) - ${comp}: Removing registry keys"
+            Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate" -Name "AccountDomainSid" -Force | Out-Null
+            Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate" -Name "PingID" -Force | Out-Null
+            Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate" -Name "SusClientId" -Force | Out-Null
+            Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate" -Name "SusClientIdValidation" -Force | Out-Null
+            Write-Output "$(Get-Date) - ${comp}: Removing SoftwareDistribution folder"
+            Remove-Item -Path C:\Windows\SoftwareDistribution -Force | Out-Null
+            Write-Output "$(Get-Date) - ${comp}: Starting Services"
+            Get-Service -Name BITS | Start-Service
+            Get-Service -Name wuauserv | Start-Service
+            Write-Output "$(Get-Date) - ${comp}: Reauthorizing client"
+            Start-Process -FilePath "C:\Windows\System32\wuauclt.exe" -ArgumentList "/resetauthorization /detectnow" -Wait
+            Start-Sleep -Seconds 10
+            Write-Output "$(Get-Date) - ${comp}: Starting detection"
+            (New-Object -ComObject Microsoft.Update.AutoUpdate).DetectNow()
+        } -ThrottleLimit 5
+    }
 }
 
 
