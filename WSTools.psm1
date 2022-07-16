ab5aa2213f5140a7f516c461d02e65c9ddbb50bf
Skyer Hart <skyler@skylerhart.com>
2022-07-15 23:14:55 -0400

    Added function Copy-UpdateHistoryReport

--- WSTools.psm1
+++ WSTools.psm1
@@ -721,6 +721,79 @@ New-Alias -Name "Update-PowerShellJSON" -Value Copy-PowerShellJSON
 New-Alias -Name "Set-PowerShellJSON" -Value Copy-PowerShellJSON
 
 
+function Copy-UpdateHistoryReport {
+<#
+.SYNOPSIS
+    Copies the UpdateHistory.csv report to the UHPath config item path.
+.DESCRIPTION
+    Copies the UpdateHistory.csv report created with Save-UpdateHistory to the UHPath config item path for the local computer or remote computers.
+.PARAMETER ComputerName
+    Specifies the name of one or more computers.
+.EXAMPLE
+    C:\PS>Copy-UpdateHistoryReport
+    Example of how to use this cmdlet to copy the UpdateHistory.csv file for the local computer to the UHPath location.
+.EXAMPLE
+    C:\PS>Copy-UpdateHistoryReport -ComputerName Server1
+    Example of how to use this cmdlet to copy the UpdateHistory.csv file for the remote computer Server1 to the UHPath location.
+.INPUTS
+    System.String
+.OUTPUTS
+    System.String
+.COMPONENT
+    WSTools
+.FUNCTIONALITY
+    UpdateHistory, update history
+.NOTES
+    Author: Skyler Hart
+    Created: 2022-07-15 22:54:09
+    Last Edit: 2022-07-15 22:54:09
+    Other:
+    Requires:
+        -RunAsAdministrator
+.LINK
+    https://wstools.dev
+#>
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+        "PSAvoidGlobalVars",
+        "",
+        Justification = "Have tried other methods and they do not work consistently."
+    )]
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            Mandatory=$false
+        )]
+        [Alias('Host','Name','Computer','CN')]
+        [string[]]$ComputerName = "$env:COMPUTERNAME"
+    )
+
+    $uhpath = ($Global:WSToolsConfig).UHPath
+    $i = 0
+    $number = $ComputerName.length
+    foreach ($Comp in $ComputerName) {
+        #Progress Bar
+        if ($number -gt "1") {
+            $i++
+            $amount = ($i / $number)
+            $perc1 = $amount.ToString("P")
+            Write-Progress -activity "Copying Update Reports. Current computer: $Comp" -status "Computer $i of $number. Percent complete:  $perc1" -PercentComplete (($i / $ComputerName.length)  * 100)
+        }#if length
+
+        if ($Comp -eq $env:COMPUTERNAME) {
+            if (Test-Path C:\ProgramData\WSTools\Report\$Comp`_UpdateHistory.csv) {
+                robocopy C:\ProgramData\WSTools\Report $uhpath *_UpdateHistory.csv /r:3 /w:5 /njh /njs | Out-Null
+            }
+            else {
+                Write-Error "Report not found. Please use Save-UpdateHistory to create a report."
+            }
+        }
+        else {
+            robocopy \\$Comp\c$\ProgramData\WSTools\Report $uhpath *_UpdateHistory.csv /r:3 /w:5 /njh /njs | Out-Null
+        }
+    }
+}
+
+
 function Copy-VSCodeExtensions {
 <#
 .NOTES
