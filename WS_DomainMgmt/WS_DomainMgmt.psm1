8b69299a39029bd16a75f714b775c1985ca07291
Skyer Hart <skyler@skylerhart.com>
2022-09-01 22:51:15 -0400

    AD module check

--- WS_DomainMgmt/WS_DomainMgmt.psm1
+++ WS_DomainMgmt/WS_DomainMgmt.psm1
@@ -229,12 +229,27 @@ Function Get-FSMO {
 .Notes
     AUTHOR: Skyler Hart
     CREATED: Sometime before 2017-08-07
-    LASTEDIT: 08/18/2017 20:48:14
+    LASTEDIT: 2022-09-01 22:47:51
     KEYWORDS:
 .LINK
     https://wstools.dev
 #>
-    netdom /query FSMO
+    if (Get-Module -ListAvailable -Name ActiveDirectory) {
+        $RoleHolders = Get-ADDomainController -Filter * | Select-Object Name,OperationMasterRoles
+        $RoleHolderInfo = foreach ($RoleHolder in $RoleHolders) {
+            $Comp = $RoleHolder.Name
+            $Roles = $RoleHolder.OperationMasterRoles
+            $Roles = $Roles -join ", "
+            [PSCustomObject]@{
+                ComputerName = $Comp
+                Roles = $Roles
+            }#new object
+        }
+        $RoleHolderInfo
+    }
+    else {
+        netdom /query FSMO
+    }
 }
 New-Alias -Name "FSMO" -Value Get-FSMO
 
