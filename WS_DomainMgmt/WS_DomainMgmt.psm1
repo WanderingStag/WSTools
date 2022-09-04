0d8b3ef3dda1be36511ef2bd7c014805362219ae
Skyer Hart <skyler@skylerhart.com>
2022-09-04 12:14:25 -0400

    Added hyper-v tools check

--- WS_DomainMgmt/WS_DomainMgmt.psm1
+++ WS_DomainMgmt/WS_DomainMgmt.psm1
@@ -845,12 +845,18 @@ Function Open-HyperVmgmt {
 .Notes
     AUTHOR: Skyler Hart
     CREATED: 08/19/2017 22:32:48
-    LASTEDIT: 08/19/2017 22:32:48
+    LASTEDIT: 2022-09-04 12:13:29
     KEYWORDS:
 .LINK
     https://wstools.dev
 #>
-    virtmgmt.msc
+    try {
+        $ErrorActionPreference = "Stop"
+        virtmgmt.msc
+    }
+    catch {
+        Write-Output "Hyper-V management tools not installed/enabled."
+    }
 }
 New-Alias -Name "hyperv" -Value Open-HyperVmgmt
 
