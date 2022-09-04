398553549b6c953b4c5d24d5db2eaf2051faf0e7
Skyer Hart <skyler@skylerhart.com>
2022-09-04 02:33:04 -0400

    AD check added to Get-ProtectedUser

--- WS_DomainMgmt/WS_DomainMgmt.psm1
+++ WS_DomainMgmt/WS_DomainMgmt.psm1
@@ -536,15 +536,20 @@ Function Get-ProtectedUser {
    .Notes
     AUTHOR: Skyler Hart
     CREATED: 02/05/2018 17:26:06
-    LASTEDIT: 02/05/2018 17:26:06
+    LASTEDIT: 2022-09-04 02:32:23
     KEYWORDS:
     REQUIRES:
         -Modules ActiveDirectory
 .LINK
     https://wstools.dev
 #>
-    $users = (Get-ADUser -filter {admincount -eq "1"}).Name | Sort-Object
-    $users
+    if (Get-Module -ListAvailable -Name ActiveDirectory) {
+        $users = (Get-ADUser -filter {admincount -eq "1"}).Name | Sort-Object
+        $users
+    }
+    else {
+        Write-Warning "Active Directory module is not installed and is required to run this command."
+    }
 }
 
 
