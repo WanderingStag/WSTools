009e2e3cee7772c75f012c740e3020ef2368365b
Skyer Hart <skyler@skylerhart.com>
2022-09-04 12:36:38 -0400

    AD check added to Restart-DNS

--- WS_DomainMgmt/WS_DomainMgmt.psm1
+++ WS_DomainMgmt/WS_DomainMgmt.psm1
@@ -1509,7 +1509,7 @@ Function Restart-DNS {
 .Notes
     AUTHOR: Skyler Hart
     CREATED: 09/08/2017 17:23:43
-    LASTEDIT: 09/08/2017 17:23:49
+    LASTEDIT: 2022-09-04 12:35:59
     KEYWORDS:
     REQUIRES:
         -Modules ActiveDirectory
@@ -1524,20 +1524,25 @@ Function Restart-DNS {
         [string]$DC = "$env:COMPUTERNAME",
         [Switch]$All
     )
-    if (!($All)) {
-        Write-Output "Restarting DNS service on $DC"
-        try {Restart-Service -inputobject $(Get-Service -ComputerName $DC -Name DNS) -Force}
-        catch {Throw "Unable to connect to $DC or failed to restart service."}
-    }#if not all
-    elseif ($All) {
-        $AllDCs = (Get-ADForest).Domains | ForEach-Object {Get-ADDomainController -Filter * -Server $_}
-        foreach ($Srv in $AllDCs) {
-            $SrvName = $Srv.HostName
-            Write-Output "Restarting DNS service on $SrvName"
-        	try {Restart-Service -inputobject $(Get-Service -ComputerName $SrvName -Name DNS) -Force}
+    if (Get-Module -ListAvailable -Name ActiveDirectory) {
+        if (!($All)) {
+            Write-Output "Restarting DNS service on $DC"
+            try {Restart-Service -inputobject $(Get-Service -ComputerName $DC -Name DNS) -Force}
             catch {Throw "Unable to connect to $DC or failed to restart service."}
-        }#foreach dc
-    }#elseif
+        }#if not all
+        elseif ($All) {
+            $AllDCs = (Get-ADForest).Domains | ForEach-Object {Get-ADDomainController -Filter * -Server $_}
+            foreach ($Srv in $AllDCs) {
+                $SrvName = $Srv.HostName
+                Write-Output "Restarting DNS service on $SrvName"
+                try {Restart-Service -inputobject $(Get-Service -ComputerName $SrvName -Name DNS) -Force}
+                catch {Throw "Unable to connect to $DC or failed to restart service."}
+            }#foreach dc
+        }#elseif
+    }
+    else {
+        Write-Warning "Active Directory module is not installed and is required to run this command."
+    }
 }
 
 
