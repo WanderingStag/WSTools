6b528c68a921f73e0e7085bb7338d4d9ee537e86
Skyer Hart <skyler@skylerhart.com>
2022-09-04 12:38:58 -0400

    AD check added to Restart-KDC

--- WS_DomainMgmt/WS_DomainMgmt.psm1
+++ WS_DomainMgmt/WS_DomainMgmt.psm1
@@ -1551,7 +1551,7 @@ Function Restart-KDC {
 .Notes
     AUTHOR: Skyler Hart
     CREATED: 08/18/2017 02:45:00
-    LASTEDIT: 08/18/2017 20:46:32
+    LASTEDIT: 2022-09-04 12:38:21
     KEYWORDS:
     REQUIRES:
         -Modules ActiveDirectory
@@ -1566,20 +1566,25 @@ Function Restart-KDC {
         [string]$DC = "$env:COMPUTERNAME",
         [Switch]$All
     )
-    if (!($All)) {
-        Write-Output "Restarting KDC service on $DC"
-        try {Restart-Service -inputobject $(Get-Service -ComputerName $DC -Name kdc) -Force}
-        catch {Throw "Unable to connect to $DC or failed to restart service."}
-    }#if not all
-    elseif ($All) {
-        $AllDCs = (Get-ADForest).Domains | ForEach-Object {Get-ADDomainController -Filter * -Server $_}
-        foreach ($Srv in $AllDCs) {
-            $SrvName = $Srv.HostName
-            Write-Output "Restarting KDC service on $SrvName"
-        	try {Restart-Service -inputobject $(Get-Service -ComputerName $SrvName -Name kdc) -Force}
+    if (Get-Module -ListAvailable -Name ActiveDirectory) {
+        if (!($All)) {
+            Write-Output "Restarting KDC service on $DC"
+            try {Restart-Service -inputobject $(Get-Service -ComputerName $DC -Name kdc) -Force}
             catch {Throw "Unable to connect to $DC or failed to restart service."}
-        }#foreach dc
-    }#elseif
+        }#if not all
+        elseif ($All) {
+            $AllDCs = (Get-ADForest).Domains | ForEach-Object {Get-ADDomainController -Filter * -Server $_}
+            foreach ($Srv in $AllDCs) {
+                $SrvName = $Srv.HostName
+                Write-Output "Restarting KDC service on $SrvName"
+                try {Restart-Service -inputobject $(Get-Service -ComputerName $SrvName -Name kdc) -Force}
+                catch {Throw "Unable to connect to $DC or failed to restart service."}
+            }#foreach dc
+        }#elseif
+    }
+    else {
+        Write-Warning "Active Directory module is not installed and is required to run this command."
+    }
 }
 
 
