67c2ade6e52705aabae16814fdda8a3b870aee91
Skyer Hart <skyler@skylerhart.com>
2022-09-04 12:12:34 -0400

    AD check added to Open-GroupPolicyMgmt

--- WS_DomainMgmt/WS_DomainMgmt.psm1
+++ WS_DomainMgmt/WS_DomainMgmt.psm1
@@ -823,12 +823,18 @@ Function Open-GroupPolicyMgmt {
 .Notes
     AUTHOR: Skyler Hart
     CREATED: 08/19/2017 22:30:09
-    LASTEDIT: 08/19/2017 22:30:09
+    LASTEDIT: 2022-09-04 12:12:07
     KEYWORDS:
 .LINK
     https://wstools.dev
 #>
-    gpmc.msc
+    try {
+        $ErrorActionPreference = "Stop"
+        gpmc.msc
+    }
+    catch {
+        Write-Output "Active Directory snapins are not installed/enabled."
+    }
 }
 New-Alias -Name "gpo" -Value Open-GroupPolicyMgmt
 New-Alias -Name "GroupPolicy" -Value Open-GroupPolicyMgmt
