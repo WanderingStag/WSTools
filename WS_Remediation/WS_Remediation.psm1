081c7d3a9f1f9f79e695662c1df6281ced606d14
Skyer Hart <skyler@skylerhart.com>
2022-09-04 21:54:07 -0400

    Added Install-GitSCM resolves #55

--- WS_Remediation/WS_Remediation.psm1
+++ WS_Remediation/WS_Remediation.psm1
@@ -5336,6 +5336,63 @@ Begin {
 }
 
 
+Function Install-GitSCM {
+<#
+   .Parameter ComputerName
+    Specifies the computer or computers
+.Notes
+    AUTHOR: Skyler Hart
+    CREATED: 2022-09-04 21:50:21
+    LASTEDIT: 2022-09-04 21:53:03
+    KEYWORDS:
+    REQUIRES:
+        -RunAsAdministrator
+.Link
+    https://wstools.dev
+#>
+    [CmdletBinding()]
+    Param (
+        [Parameter()]
+        [Alias('Host','Name','Computer','CN')]
+        [string[]]$ComputerName = "$env:COMPUTERNAME"
+    )
+
+    $config = $Global:WSToolsConfig
+    $app = $config.Git
+
+    $b = 0
+    $n = $ComputerName.Count
+    foreach ($comp in $ComputerName) {
+        if ($n -gt 1) {
+            $b++
+            $p = ($b / $n)
+            $p1 = $p.ToString("P")
+            Write-Progress -Id 1 -activity "Copying Git SCM to computer and then initiating install" -status "Computer $b of $n. Percent complete:  $p1" -PercentComplete (($b / $n)  * 100)
+        }
+
+        try {
+            robocopy $app \\$comp\c$\Patches\GitSCM /mir /mt:2 /r:3 /w:15 /njh /njs
+            $install = Invoke-WMIMethod -Class Win32_Process -ComputerName $comp -Name Create -ArgumentList "cmd /c c:\Patches\GitSCM\Git-64-bit.exe /SP- /VERYSILENT /SUPPRESSMSGBOXES /NOCANCEL /NORESTART /CLOSEAPPLICATIONS /NORESTARTAPPLICATIONS /TYPE=full" -ErrorAction Stop #DevSkim: ignore DS104456
+            $end = Get-Date
+            $info = [PSCustomObject]@{
+                ComputerName = $comp
+                Status = "Install Initialized"
+                Time = $end
+            }#new object
+        }
+        catch {
+            $end = Get-Date
+            $info = [PSCustomObject]@{
+                ComputerName = $comp
+                Status = "Unable to install"
+                Time = $end
+            }#new object
+        }
+        $info
+    }
+}
+
+
 Function Install-MECM {
 <#
 .Notes
