72614cc8ebfa5250f829de31aab7c2ac7bf9ae93
Skyer Hart <skyler@skylerhart.com>
2022-09-20 20:16:40 -0400

    Added function Set-ChromeDeveloperTools

--- WSTools.psm1
+++ WSTools.psm1
@@ -4967,6 +4967,80 @@ function Set-AxwayConfig {
 New-Alias -Name "Import-AxwayConfig" -Value Set-AxwayConfig
 
 
+function Set-ChromeDeveloperTools {
+<#
+.SYNOPSIS
+    Will enable or disable Chrome Developer tools.
+.DESCRIPTION
+    Sets the registry entry HKLM:\SOFTWARE\Policies\Google\Chrome\DeveloperToolsDisabled to 1 (Disabled) or 0 (Enabled)
+.PARAMETER Disable
+    Will Disable Chrome Developer Tools.
+.EXAMPLE
+    C:\PS>Set-ChromeDeveloperTools
+    Example of how to use this cmdlet to enable Chrome Developer Tools.
+.EXAMPLE
+    C:\PS>Set-ChromeDeveloperTools -Disable
+    Example of how to use this cmdlet to disable Chrome Developer Tools.
+.INPUTS
+    System.String
+.OUTPUTS
+    System.Management.Automation.PSCustomObject
+.COMPONENT
+    WSTools
+.FUNCTIONALITY
+    Chrome, Developer Tools
+.NOTES
+    Author: Skyler Hart
+    Created: 2022-09-20 19:53:22
+    Last Edit: 2022-09-20 19:53:22
+    Other:
+    Requires:
+        -RunAsAdministrator
+.LINK
+    https://wstools.dev
+#>
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute(
+        "PSUseSingularNouns",
+        "",
+        Justification = "Developer Tools is the actual name of the setting so keeping it consistent."
+    )]
+    [CmdletBinding()]
+    param(
+        [Parameter()]
+        [switch]$Disable
+    )
+
+    $currentPrincipal = New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())
+    if ($currentPrincipal.IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+        if ($Disable) {
+            try {
+                Get-ItemPropertyValue -Path HKLM:\SOFTWARE\Policies\Google\Chrome -Name DeveloperToolsDisabled -ErrorAction Stop
+
+                #modify entry
+                Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Google\Chrome -Name DeveloperToolsDisabled -Value 1
+            }
+            catch {
+                #Create entry
+                New-ItemProperty -Path HKLM:\SOFTWARE\Policies\Google\Chrome -Name DeveloperToolsDisabled -Value 1
+            }
+        }
+        else {
+            try {
+                Get-ItemPropertyValue -Path HKLM:\SOFTWARE\Policies\Google\Chrome -Name DeveloperToolsDisabled -ErrorAction Stop
+                Set-ItemProperty -Path HKLM:\SOFTWARE\Policies\Google\Chrome -Name DeveloperToolsDisabled -Value 0
+            }
+            catch {
+                Write-Output "Chrome Developer Tools already enabled."
+            }
+        }
+    }
+    else {
+        Write-Warning "Function must be ran as administrator."
+    }
+}
+New-Alias -Name "Set-DeveloperTools" -Value Set-ChromeDeveloperTools
+
+
 Function Set-Explorer {
 <#
    .Notes
