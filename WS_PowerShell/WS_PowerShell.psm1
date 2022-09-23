ca0e02d3af67fd60d1dd3591affde4f951042644
Skyer Hart <skyler@skylerhart.com>
2022-09-22 23:52:09 -0400

    Added function Get-PowerShellVariable

--- WS_PowerShell/WS_PowerShell.psm1
+++ WS_PowerShell/WS_PowerShell.psm1
@@ -845,6 +845,62 @@ Function Get-FunctionsInModule {
 }
 
 
+function Get-PowerShellVariable {
+<#
+.SYNOPSIS
+    Will show env: and PowerShell variable active in session.
+.DESCRIPTION
+    Gets environment variables and the active PowerShell variables in the current session and shows their values.
+.PARAMETER Name
+    To filter for a specific variable.
+.EXAMPLE
+    C:\PS>Get-PowerShellVariable
+    Example of how to use this cmdlet.
+.EXAMPLE
+    C:\PS>Get-PowerShellVariable -Name ErrorActionPreference
+    Will show what the value is for $ErrorActionPreference.
+.EXAMPLE
+    C:\PS>Get-PowerShellVariable -Name ErrorActionPreference,OneDriveConsumer
+    Will show what the value is for $ErrorActionPreference and $env:OneDriveConsumer.
+.INPUTS
+    System.String
+.OUTPUTS
+    System.Management.Automation.PSCustomObject
+.COMPONENT
+    WSTools
+.FUNCTIONALITY
+    variable, environment, system
+.NOTES
+    Author: Skyler Hart
+    Created: 2022-09-22 23:29:51
+    Last Edit: 2022-09-22 23:29:51
+    Other:
+    Requires:
+.LINK
+    https://wstools.dev
+#>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory=$false)]
+        [string[]]$Name
+    )
+
+    $variables = Get-ChildItem Env: | Add-Member -MemberType NoteProperty -Name "Type" -Value "`$env:" -PassThru
+    $variables += Get-Variable | Add-Member -MemberType NoteProperty -Name "Type" -Value "PowerShell" -PassThru
+
+    if (!([string]::IsNullOrWhiteSpace($Name))) {
+        $filtered = foreach ($obj in $Name) {
+            $variables | Where-Object {$_.Name -match $obj} | Select-Object Type,Name,Value
+        }
+    }
+    else {
+        $filtered = $variables | Select-Object Type,Name,Value
+    }
+
+    $filtered | Select-Object -Unique | Sort-Object Name
+}
+
+
 Function Get-Role {
 <#
 .Notes
