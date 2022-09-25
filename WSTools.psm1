9f7e3a36a873c9eaed36bc40ea1e927ca89479aa
Skyer Hart <skyler@skylerhart.com>
2022-09-25 01:45:16 -0400

    Added function Sync-InTune with alias Sync-MEM

--- WSTools.psm1
+++ WSTools.psm1
@@ -6495,6 +6495,41 @@ function Stop-Exchange {
 }
 
 
+function Sync-InTune {
+<#
+.SYNOPSIS
+    Will sync device with InTune/MEM.
+.DESCRIPTION
+    Will initiate the sync process with InTune/Microsoft EndPoint Manager to receive new policies and report information.
+.EXAMPLE
+    C:\PS>Sync-InTune
+    Example of how to use this cmdlet.
+.COMPONENT
+    WSTools
+.FUNCTIONALITY
+    InTune, Microsoft Endpoint Manager, MEM
+.NOTES
+    Author: Skyler Hart
+    Created: 2022-09-25 01:38:28
+    Last Edit: 2022-09-25 01:38:28
+    Other:
+    Requires:
+.LINK
+    https://wstools.dev
+#>
+    [CmdletBinding()]
+    param()
+
+    try {
+        Get-ScheduledTask -TaskName PushLaunch -ErrorAction Stop | Start-ScheduledTask
+    }
+    catch {
+        Write-Warning "Device is not InTune/Microsoft Endpoint Manager (MEM) managed."
+    }
+}
+New-Alias -Name "Sync-MEM" -Value Sync-InTune
+
+
 Function Test-EmailRelay {
     <#
        .Notes
