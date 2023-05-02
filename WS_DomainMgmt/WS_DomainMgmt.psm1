166580d5dd92c3a419f61b794bb142508022e045
Skyler Hart <skyler.hart.1.ctr@us.af.mil>
2023-05-02 17:48:02 -0400

    Added function Get-NonSmartCardRequiredUser

--- WS_DomainMgmt/WS_DomainMgmt.psm1
+++ WS_DomainMgmt/WS_DomainMgmt.psm1
@@ -380,6 +380,68 @@ Function Get-NewADGroup {
 }
 
 
+function Get-NonSmartCardRequiredUser {
+    <#
+    .SYNOPSIS
+        Displays users in domain with SmartCardRequired attribute set to false.
+
+    .DESCRIPTION
+        Displays all users in the domain with SmartCardRequired attribute on account set to false.
+
+    .PARAMETER ComputerName
+        Specifies the name of one or more computers.
+
+    .EXAMPLE
+        C:\PS>Get-NonSmartCardRequiredUser
+        Example of how to use this cmdlet
+
+    .INPUTS
+        None
+
+    .OUTPUTS
+        System.Array
+
+    .COMPONENT
+        WSTools
+
+    .FUNCTIONALITY
+        Active Directory, Smartcard, Smart Card, InTh, Insider Threat
+
+    .NOTES
+        Author: Skyler Hart
+        Created: 2023-05-02 17:16:53
+        Last Edit: 2023-05-02 17:16:53
+        Requires:
+            -Module ActiveDirectory
+
+    .LINK
+        https://wstools.dev
+    #>
+    [CmdletBinding()]
+    Param (
+        [AllowEmptyString()]
+        [Alias('User')]
+        [string]$Name
+    )
+
+    Begin {
+        $ErrorActionPreference = "Stop"
+        if ($null -eq (Get-Module -ListAvailable ActiveDir*).Path) {
+            throw "Active Directory module not found. Active Directory module is required to run this function."
+        }
+    }
+    Process {
+        $users = Get-ADUser -Filter {SmartCardLogonRequired -eq $false} -Properties SmartCardLogonRequired,DisplayName,CanonicalName
+    }
+    End {
+        if ($Name) {
+            $users | Where-Object {$_ -match $Name}
+        }
+        else {$users}
+    }
+}
+
+
 Function Get-PrivilegedGroup {
 <#
 .Notes
