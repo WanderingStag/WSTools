fc16678d2861c0a80b98188de4a6a82e367b35d1
Skyler Hart <skyler.hart.1.ctr@us.af.mil>
2023-11-18 11:42:09 -0500

    Bumped version Added Apache to AppNames

--- config.ps1
+++ config.ps1
@@ -5,11 +5,11 @@
 )]
 
 $Global:WSToolsConfig = [PSCustomObject]@{
-    # WSTools config v2023.5
+    # WSTools config v2023.11
     # Remove the # symbol infront of a line to enable it
     ###########################################################################################################################################
     #Application Service Names - used in Stop-AppService. Uses match to match the name of a service that is running and stops it.
-    AppNames = @('BarTender','Cognos','Commvault','Dameware','Exchange','IBM','IDenticard','IIS','InstallRoot','LiveLink','Moodle','Oracle','Seagull','SharePoint','SolarWinds','Splunk','SQL','Tanium','Tomcat','Tumbleweed','World Wide Web','WSUS')
+    AppNames = @('Apache','BarTender','Cognos','Commvault','Dameware','Exchange','IBM','IDenticard','IIS','InstallRoot','LiveLink','Moodle','Oracle','Seagull','SharePoint','SolarWinds','Splunk','SQL','Tanium','Tomcat','Tumbleweed','World Wide Web','WSUS')
 
     #Ignore list. Used in several functions. Computers, users, and groups to ignore that cause issues. Objects such as clustered object computer names, non-windows systems, and other such things. Uses -match in some places and -eq in others so be as precise as possible.
     #Ignore = @('comp1','comp2','user1','user2','group1','group2')
